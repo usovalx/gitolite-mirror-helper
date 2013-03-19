dca7a1bb6ff1838d32f3f5137f3eccdbae8ce82c
Oleksandr Usov <a.s.usov@gmail.com>
2013-03-19 10:15:20 +0000

    signal handling todo

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+        "os/signal"
 	"time"
 )
 
@@ -13,9 +14,22 @@ func main() {
 	fmt.Printf("Starting\n")
 
 	stopCh := make(chan bool)
-	resCh := ProcMon(stopCh, mainLogger, "echo", []string{"false", "-N", "repo-au"})
+	resCh := ProcMon(stopCh, mainLogger, "echo", []string{"ssh", "-N", "repo-au"})
 
-	time.Sleep(10 * time.Second)
+        // FIXME: signal handling to shutdown child processes
+        // HUP -> reload config & restart
+        // INT -> shutdown
+        // TERM -> shutdown
+        sigCh := make(chan os.Signal, 10)
+        signal.Notify(sigCh, os.Interrupt)
+        go func() {
+            for {
+                s := <-sigCh
+                mainLogger.Printf("Signal: %d", s)
+            }
+        } ()
+
+	time.Sleep(100 * time.Second)
 	stopCh <- true
 	_ = <-resCh
 }
