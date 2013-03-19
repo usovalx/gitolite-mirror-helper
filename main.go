490281c198660921ff9a7cca5f5d9e0e1adb2ae6
Oleksandr Usov <a.s.usov@gmail.com>
2013-03-19 10:15:06 +0000

    more tests

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
+	"time"
 )
 
+var mainLogger = log.New(os.Stderr, "", log.LstdFlags)
+
 func main() {
-    fmt.Printf("Hello\n")
-    procmon(
-        log.New(os.Stderr, "", log.LstdFlags),
-        "echo",
-        []string{"echo", "foo", "bar"})
+	fmt.Printf("Starting\n")
+
+	stopCh := make(chan bool)
+	resCh := ProcMon(stopCh, mainLogger, "echo", []string{"false", "-N", "repo-au"})
+
+	time.Sleep(10 * time.Second)
+	stopCh <- true
+	_ = <-resCh
 }
--- procmon.go
+++ procmon.go
@@ -5,31 +5,108 @@
 package main
 
 import (
-    "log"
-    //"fmt"
-    "os/exec"
+	"log"
+	"os"
+	"os/exec"
+	"time"
 )
 
-type Logger struct {
-    prefix string
-    l *log.Logger
+const (
+	minDelay          = 1 * time.Second
+	maxDelay          = 3 * time.Minute
+	goodTimeThreshold = 30 * time.Second
+)
+
+func ProcMon(dieCh <-chan bool, logger *log.Logger, ident string, args []string) <-chan bool {
+	c := make(chan bool, 1)
+	go procMonLoop(dieCh, c, logger, ident, args)
+	return c
 }
 
-// Write a message to log
-func (l *Logger) Write(b []byte) (n int, err error) {
-    buf := b
-    if buf[len(b)-1] == '\n' {
-        buf = b[:len(b)-1]
-    }
+func procMonLoop(dieCh <-chan bool, doneCh chan<- bool, logger *log.Logger,
+	ident string, args []string) {
 
-    l.l.Printf("%s: %s", l.prefix, buf)
-    return len(b), nil
+	defer func() { doneCh <- true }()
+
+	stop := false
+	startDelay := time.Duration(0)
+	slaveDied := make(chan bool, 1)
+	cmd := (*exec.Cmd)(nil)
+	for !stop {
+		// part 1 -- starting new slave process
+		t := time.NewTimer(startDelay)
+		select {
+		case _ = <-dieCh:
+			t.Stop()
+			return
+		case _ = <-t.C:
+			logger.Printf("%s: starting %s", ident, args)
+			cmd, slaveDied = startSlave(logger, ident, args)
+		}
+
+	waitForIt:
+		// part 2 -- monitoring running slave process
+		select {
+		case _ = <-dieCh:
+			stop = true
+			cmd.Process.Signal(os.Interrupt)
+			goto waitForIt
+		case r := <-slaveDied:
+			if r {
+				startDelay = minDelay
+			} else {
+				startDelay *= 2 // exponential back-off
+				if startDelay < minDelay {
+					startDelay = minDelay
+				}
+				if startDelay > maxDelay {
+					startDelay = maxDelay
+				}
+			}
+			logger.Printf("%s: slave died, restarting in %8.3f seconds", ident, float64(startDelay)/float64(time.Second))
+			cmd = nil
+		}
+	}
+	return
 }
 
-func procmon(logger *log.Logger, ident string, command []string) {
-    c := exec.Command(command[0], command[1:]...)
-    c.Stdout = &Logger{ident + ".stdout", logger}
-    c.Stderr = &Logger{ident + ".stderr", logger}
+func startSlave(logger *log.Logger, ident string, args []string) (cmd *exec.Cmd, ch chan bool) {
+	cmd = exec.Command(args[0], args[1:]...)
+	cmd.Stdin = nil
+	cmd.Stdout = &LogWriter{ident + ".stdout", logger}
+	cmd.Stderr = &LogWriter{ident + ".stderr", logger}
 
-    c.Run()
+	ch = make(chan bool, 1)
+	err := cmd.Start()
+	if err != nil {
+		logger.Printf("%s: start: %s", ident, err)
+		ch <- false
+	} else {
+		go func() {
+			startTime := time.Now()
+			err := cmd.Wait()
+			if err != nil {
+				logger.Printf("%s: wait: %s", ident, err)
+			}
+			// FIXME: stats?
+			//logger.Printf("%s: finished: %s", ident, cmd.ProcessState)
+			ch <- (time.Since(startTime) > goodTimeThreshold)
+		}()
+	}
+	return
+}
+
+type LogWriter struct {
+	ident  string
+	logger *log.Logger
+}
+
+func (l *LogWriter) Write(b []byte) (n int, err error) {
+	n = len(b)
+	if b[n-1] == '\n' {
+		b = b[:n-1]
+	}
+
+	l.logger.Printf("%s: %s", l.ident, b)
+	return n, nil
 }
